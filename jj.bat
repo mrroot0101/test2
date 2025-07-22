bc2e7bf700ef54110925c93d56dda518de09dcfa
mrroot0101 <pes1pg23ca344@gmail.com>
2025-07-22 19:11:56 +0530

    Create jj.bat

---
+++ jj.bat
@@ -0,0 +1,12 @@
+@echo off
+title Test Message
+echo #######################################
+echo #                                   #
+echo #             H A C K E D           #
+echo #                                   #
+echo #######################################
+echo.
+echo This is a test message. Nothing malicious.
+echo.
+pause
+exit
